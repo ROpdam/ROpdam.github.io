e0cc1807a0a2584b5088ae68b9b89add4b6435b5
ROpdam <robinopdam@hotmail.com>
2020-09-07 12:17:27 +0200

    Profile pic shrinks when screen size < 800px

--- javascript/custom.js
+++ javascript/custom.js
@@ -5,10 +5,3 @@ $(".panel-heading").parent('.panel').hover(
     $(this).children('.collapse').collapse('hide');
   }
 );
-$(document).ready(function() {
-  $('.template-article img').each(function() {
-      var currentImage = $(this);
-      currentImage.wrap("<a class='image-link' href='" + currentImage.attr("src") + "'</a>");
-  });
-  $('.image-link').magnificPopup({type:'image'});  
-});
