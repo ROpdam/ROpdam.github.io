90834b37982df1ae32f95f25a5314827282a0313
ROpdam <robinopdam@hotmail.com>
2020-10-08 10:44:40 +0200

    Navbar collapse on link click

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,3 +1,8 @@
+// Collapse navbar on link click
+$('.navbar-nav>li>a').on('click', function(){
+  $('.navbar-collapse').collapse('hide');
+});
+
 $(".panel-heading").parent('.panel').hover(
   function() {
     $(this).children('.collapse').collapse('show');
